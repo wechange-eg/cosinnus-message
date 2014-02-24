55ec182fd586d1e2b0d4cf88bf3a6f31decea2f4
Sascha Narr <saschanarr@googlemail.com>
2014-02-24 17:28:34 +0100

    Added validation for missing recipients

--- cosinnus_message/forms.py
+++ cosinnus_message/forms.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 
 from django.forms.models import ModelForm
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
 from cosinnus_message.models import Message
 from cosinnus.views.mixins.group import GroupFormKwargsMixin
+from django.core.exceptions import ValidationError
 
 class MessageForm(ModelForm, GroupFormKwargsMixin):
 
@@ -21,6 +23,8 @@ class MessageForm(ModelForm, GroupFormKwargsMixin):
         """ override recipient selection if broadcast was selected and send to ALL group members """
         recipients = self.cleaned_data['recipients']
         broadcast = self.cleaned_data['isbroadcast']
+        if not broadcast and not recipients:
+            raise ValidationError(_('Please select a recipient for the message!'), code='missing_recipient')
         if broadcast:
             recipients = self.fields['recipients'].queryset.all()
 
