d8a6507e9532093eedf58ea7d753bfccb52749ce
Sascha Narr <saschanarr@googlemail.com>
2014-10-22 18:05:24 +0200

    Postman default settings added to cosinnus_message.conf

--- cosinnus_message/conf.py
+++ cosinnus_message/conf.py
@@ -7,4 +7,17 @@ from appconf import AppConf
 
 
 class CosinnusMessageConf(AppConf):
-    SHOW_MESSAGE_SENDER_EMAIL = False
+    pass
+
+
+class CosinnusMessageDefaultSettings(AppConf):
+    """ Settings without a prefix namespace to provide default setting values for other apps.
+        These are settings used by default in cosinnus apps, such as avatar dimensions, etc.
+    """
+    
+    class Meta:
+        prefix = ''
+        
+    POSTMAN_DISALLOW_ANONYMOUS = True  # No anonymous messaging
+    POSTMAN_AUTO_MODERATE_AS = True  # Auto accept all messages
+    POSTMAN_SHOW_USER_AS = 'username'
--- cosinnus_message/models.py
+++ cosinnus_message/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from cosinnus_message.conf import settings # we need this import here!
 
 import django
 if django.VERSION[:2] < (1, 7):
