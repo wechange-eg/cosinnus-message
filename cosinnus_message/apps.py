33bddf3df91749de98fb43a5a7edcd976231d784
Sascha Narr <saschanarr@googlemail.com>
2015-11-02 17:13:25 +0100

    Django 1.8 app loading

---
+++ cosinnus_message/apps.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class CosinnusMessageAppConfig(AppConfig):
+
+    name = 'cosinnus_message'
+    verbose_name = 'Cosinnus Message'
+
+    def ready(self):
+        from cosinnus_message import cosinnus_app
+        cosinnus_app.register()
+
