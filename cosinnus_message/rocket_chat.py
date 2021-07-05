6d53d3d7fba93ac25c285306571e04ee7f79ae17
Sascha Narr <saschanarr@gmail.com>
2021-07-05 12:28:08 +0200

    Rocketchat: @all instead of @username for new contact group rooms

--- cosinnus_message/rocket_chat.py
+++ cosinnus_message/rocket_chat.py
@@ -605,7 +605,7 @@ class RocketChatConnection:
             # Post help message
             greeting_message = group.trans.CONTACT_ROOM_GREETING_MESSAGE
             info_message = _('Please post your request or question here. You can see the channel members by clicking the group icon at the top of the channel.')
-            message = f'@{profile.rocket_username} {greeting_message} {info_message}'
+            message = f'@all {greeting_message} {info_message}'
             response = self.rocket.chat_post_message(text=message, room_id=room_id).json()
             if not response.get('success'):
                 logger.error('RocketChat: groups_request: chat_post_message ' + response.get('errorType', '<No Error Type>'), extra={'response': response})
